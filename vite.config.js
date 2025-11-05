bcd74867b58b7de6ca08416d793236f6148f9d86
wanjiasu <11630918+wanjiasu@user.noreply.gitee.com>
2025-11-05 13:05:51 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext', // ensure top-level await stays in the output
+  },
 })
   
